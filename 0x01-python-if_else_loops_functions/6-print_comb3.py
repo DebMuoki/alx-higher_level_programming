fc2e5c935e65b8ef58f475a2b0412eda09de9606
DebMuoki <ndanumuokideb@gmail.com>
2023-10-03 13:41:39 -0700

    Task six

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 
 for digit1 in range(10):
     for digit2 in range(digit1 + 1, 10):
-        print("{:d}{:d}".format(digit1, digit2), end=", ")
-
-print()
+            if digit1 == 8 and digit2 == 9:
+                print("{}{}".format(digit1, digit2))
+            else:
+                print("{}{}".format(digit1, digit2), end=", ")
