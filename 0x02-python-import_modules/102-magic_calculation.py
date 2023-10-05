02bf2aa68eb59407e1075591fc5a4d383ec9f313
DebMuoki <ndanumuokideb@gmail.com>
2023-10-05 10:31:12 -0700

    Task eight

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+def magic_calculation(a, b):
+    if a < b:
+        from magic_calculation_102 import add
+        c = add(a, b)
+        for i in range(4, 7):
+            c = add(c, i)
+        return c
+    else:
+        from magic_calculation_102 import sub
+        return sub(a, b)
