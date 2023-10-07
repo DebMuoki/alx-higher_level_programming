230d8dbc4edbe43b09874cca6053997219843416
DebMuoki <ndanumuokideb@gmail.com>
2023-10-07 11:44:45 -0700

    Task ten

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 10-divisible_by_2.py
+
+def divisible_by_2(my_list=[]):
+
+    result = []
+
+    for num in my_list:
+
+        if num % 2 == 0:
+            result.append(True)
+        else:
+            result.append(False)
+
+    return result
