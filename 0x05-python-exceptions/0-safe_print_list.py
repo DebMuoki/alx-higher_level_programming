4623b9baf8b6f95fbeba41e6a9dd34f9fc7b8509
DebMuoki <ndanumuokideb@gmail.com>
2023-10-23 03:53:39 -0700

    Task zero

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    try:
-        for i in range(x):
+
+    for i in range(x):
+        if i < len(my_list):
             print(my_list[i], end="")
             printed += 1
-    except IndexError:
-        pass
+
     print()
     return printed
