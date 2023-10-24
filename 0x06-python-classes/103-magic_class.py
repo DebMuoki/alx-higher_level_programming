58ba40b44653d0ec182ecd0ddf5c93a9b99434ec
DebMuoki <ndanumuokideb@gmail.com>
2023-10-24 10:33:30 -0700

    Task Ten

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
+"""
+Module docstring:
+This module defines a MagicClass that represents a circle.
+"""
+
 import math
 
 
 class MagicClass:
     """
+    Class docstring:
     This class defines a magic circle by radius.
     """
 
     def __init__(self, radius=0):
         """
+        Method docstring:
         This method initializes the magic circle.
+        Checks if the radius is a number int or float, else raises  TypeError.
+        If the radius is less than 0, it raises a ValueError.
         """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
@@ -18,12 +27,14 @@ class MagicClass:
 
     def area(self):
         """
-        This method calculates the area of the magic circle.
+        Method docstring:
+        This method calculates and returns the area of the magic circle.
         """
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """
-        This method calculates the circumference of the magic circle.
+        Method docstring:
+        Calculates and returns the circumference of the magic circle.
         """
         return 2 * math.pi * self.__radius
