235f191cf39b8dc780945130ebb2fa9768f2729a
DebMuoki <ndanumuokideb@gmail.com>
2023-10-24 07:30:21 -0700

    Task zero

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+Its purpose is to provide a blueprint for creating square objects.
+"""
+
 
 class Square:
     """
-    This is the Square class.
+    This is a Square class.
+    It defines a square but currently does not have any attributes or methods.
     """
     pass
